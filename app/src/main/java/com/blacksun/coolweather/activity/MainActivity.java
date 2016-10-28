21133f2cfc1c867866c5c49e6ef08fb0bff7d96d
V-sign <blacksun_dl@163.com>
2016-10-29 03:21:44 +0800

    新增数据库帮助类，以及各表对应的实体类。

--- app/src/main/java/com/blacksun/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/blacksun/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.blacksun.coolweather;
+package com.blacksun.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blacksun.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
